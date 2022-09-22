877e8606fd0c3c54bd81bcfce44ba14b2a0a5715
yummyhotpot <a25580819a1107@gmail.com>
2022-09-22 15:22:18 +0800

    加入專案檔案。

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            char [] delimiterChars = { ' ', ',', ',', '，', '。',':', '\t', '.' };
+            string[] words = textBox.Text.Split(delimiterChars);
+            string ans = "";
+            string[] split = textBox2.Text.Split(delimiterChars);
+
+            foreach (var word in words)
+            {
+                ans += $"{word}";
+            }
+
+            textBox1.Text = "";
+
+            foreach (var word1 in split)
+            {
+                char aa = Convert.ToChar(word1);
+                int count = ans.Split(aa).Length - 1;
+                textBox1.Text += $"{word1}:{count}\n";
+            }
+        }
+    }
+}
